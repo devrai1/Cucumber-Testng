29ac600df06304ade971a64e8b502c25e076098f
dev1 <devrai914@gmail.com>
2025-07-22 15:54:14 +0530

    lab login commit

--- src/test/java/page/DashboardPage.java
+++ src/test/java/page/DashboardPage.java
@@ -1,5 +1,15 @@
 package page;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class DashboardPage {
+	
+	WebDriver driver;
+	// to initialize the webElement we use this constructor
+		public DashboardPage(WebDriver driver) {
+	        this.driver = driver;
+	        PageFactory.initElements(driver, this); // 💡 REQUIRED!
+	    }
 
 }
